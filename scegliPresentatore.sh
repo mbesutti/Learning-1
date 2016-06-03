f496f4ae4e1b94715b264cf75296d7a3d5334a7c
kolch182 <stefanocoluccia@hotmail.com>
2016-06-03 15:29:20 +0200

    Update scegliPresentatore.sh

--- scegliPresentatore.sh
+++ scegliPresentatore.sh
@@ -2,19 +2,46 @@ expressions=("Dario" "Stefano" "Valentino" "Vacca" "Gennaro")
 RANDOM=$(( ( RANDOM % 10 )  + 1 ))
 
 echo
-echo "Premere INVIO per scegliere il presentatore di oggi"
+echo "Premere INVIO per scegliere il presentatore di oggi, premere + per aggiungere, - per rimuovere"
+echo "I candidati presentatori sono al momento:"
+echo
+echo "${expressions[@]}"
 echo
 
 read -s -n 1 key
 
-while [[ $key = "" ]]
-do
-	if [[ $key = "" ]]; then 
-		selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
-		echo "*****  "$selectedexpression"  *****"
-		echo
-		echo "Premere INVIO per scegliere un altro presentatore, altrimenti premi un altro tasto"
-		read -s -n 1 key
-		echo
-	fi
-done
+case $key in
+	"")
+#if [[ $key = "" ]]; then
+	while [[ $key = "" ]]
+	do
+		if [[ $key = "" ]]; then 
+			selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
+			echo "*****  "$selectedexpression"  *****"
+			echo
+			echo "Premere INVIO per scegliere un altro presentatore, altrimenti premi un altro tasto"
+			read -s -n 1 key
+			echo
+		fi
+	done
+	;
+	"+")
+
+#else 
+
+	echo "Inserisci un nuovo possibile presentatore:"
+	read  key
+	expressions+=("$key")
+	echo "I candidati presentatori sono al momento:"
+	echo
+	echo "${expressions[@]}"
+	echo	
+	;
+
+	"-")
+	echo "meno"	
+	;	
+	*)
+	exit
+esac
+#fi
