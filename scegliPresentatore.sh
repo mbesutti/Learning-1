4e6a591bb13cf54009cc73565d578e19e495010f
kolch182 <stefanocoluccia@hotmail.com>
2016-06-04 18:38:30 +0200

    Update scegliPresentatore.sh

--- scegliPresentatore.sh
+++ scegliPresentatore.sh
@@ -1,47 +1,78 @@
-expressions=("Dario" "Stefano" "Valentino" "Vacca" "Gennaro")
+#!/bin/bash
+
+expressions=("DARIO" "STEFANO" "VALENTINO" "VACCA" "GENNARO")
 RANDOM=$(( ( RANDOM % 10 )  + 1 ))
 
-echo
-echo "Premere INVIO per scegliere il presentatore di oggi, premere + per aggiungere, - per rimuovere"
-echo "I candidati presentatori sono al momento:"
-echo
-echo "${expressions[@]}"
-echo
+function scegli {
+	selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
+	echo	
+	echo "*****  "$selectedexpression"  *****"
+	echo
+}
 
-read -s -n 1 key
-
-case $key in
-	"")
-#if [[ $key = "" ]]; then
-	while [[ $key = "" ]]
-	do
-		if [[ $key = "" ]]; then 
-			selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
-			echo "*****  "$selectedexpression"  *****"
-			echo
-			echo "Premere INVIO per scegliere un altro presentatore, altrimenti premi un altro tasto"
-			read -s -n 1 key
-			echo
-		fi
-	done
-	;
-	"+")
-
-#else 
-
+function aggiungi {
 	echo "Inserisci un nuovo possibile presentatore:"
 	read  key
-	expressions+=("$key")
+	expressions+=("${key^^}")
 	echo "I candidati presentatori sono al momento:"
 	echo
 	echo "${expressions[@]}"
 	echo	
-	;
+}
 
-	"-")
-	echo "meno"	
-	;	
-	*)
-	exit
-esac
-#fi
+function rimuovi {
+	echo "Rimuovi un nuovo possibile presentatore:"
+	read  key
+	trim=${key//[[:blank:]]/}
+	for i in "${!expressions[@]}"; do
+		if [[ "${expressions[$i]}" = "${trim^^}" ]]; then
+			#unset expressions["${i}"]
+			expressions=(${expressions[@]:0:$i} ${expressions[@]:$(expr $i + 1)})
+			trovato=1
+		fi
+	done
+	if [[ "$trovato" -ne 1 ]]; then
+		echo
+		echo "!!!ATTENZIONE!!! Il presentatore che si è cercato di rimuovere non esiste!"
+		echo
+	fi
+	echo "I candidati presentatori sono al momento:"
+	echo
+	echo "${expressions[@]}"
+	echo	
+}
+
+echo
+echo "I candidati presentatori sono al momento:"
+echo
+#echo "${expressions[@]}"
+printf "* "'%s\n' "${expressions[@]}"
+echo
+echo "Durante l'esecuzione dello script usa una delle seguenti opzioni:"
+echo
+echo "****************************************************"
+echo "Premere INVIO per scegliere il presentatore di oggi"
+echo "Premere + per aggiungere un presentatore"
+echo "Premere - per rimuovere un presentatore"
+echo "Premere q per terminare l'esecuzione"
+echo "****************************************************"
+echo
+
+while [[ $key != "quit" ]]
+	do
+	read -s -n 1 key
+
+	case $key in
+		"")
+		scegli
+		;;
+		"+")
+		aggiungi
+		;;
+		"-")
+		rimuovi
+		;;
+		"q")
+		exit	
+	esac
+done
