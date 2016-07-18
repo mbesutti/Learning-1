39f38b4ea46a0b404ea4b5a15ef2bfc8d4900e37
kolch182 <stefanocoluccia@hotmail.com>
2016-07-18 15:13:56 +0200

    Aggiunti i test

---
+++ Lezione 3/SeminarRefactored/src/stefano/seminar/Student.java
@@ -0,0 +1,16 @@
+package stefano.seminar;
+
+public class Student {
+
+	private String _name;
+	private String _lastname;
+	
+	public Student(String name, String lastname) {
+		_name = name;
+		_lastname = lastname;
+	}
+
+	public String getInfo() {
+		return _name + " " + _lastname;
+	}
+}
